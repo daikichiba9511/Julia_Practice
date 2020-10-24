0ab115052637ec5689f936c6a90dcffa894d49de
daikichiba9511 <daikichiba.hokkaido@gmail.com>
2020-10-24 23:57:32 +0900

    update

--- Practice/src/make_data.jl
+++ Practice/src/make_data.jl
@@ -2,9 +2,16 @@ using DataFrames
 using CSV
 
 function confirm_data_description()
-    csv = DataFrame(CSV.read("./input/cv-valid-train.csv"))
-    @show head(csv[[:text, :filename]])
+    df = DataFrame(CSV.read("./input/cv-valid-train.csv"))
+    @show head(df)
+    # if there are Missing, it doesnt seem to work.
+    # println(head(filter(row -> row[:accent] .== "us", df)))
+
+    # so, use 'isequal' instead of '=='
+    println(head(filter(:accent => isequal("us"), df)))
 end
 
 confirm_data_description()
+
+
 
