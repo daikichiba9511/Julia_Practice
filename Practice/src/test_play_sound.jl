072bc097f235239936b58c374fc136c8ad100a5c
daikichiba9511 <daikichiba.hokkaido@gmail.com>
2020-11-05 00:29:09 +0900

    update spectrogram code

--- Practice/src/test_play_sound.jl
+++ Practice/src/test_play_sound.jl
@@ -6,16 +6,20 @@ using DSP
 using MFCC
 
 # for vscode
+# plotly()
+# popdisplay()
 gr()
+
 
 function plot_snd(load_filepath::String)
     snd, fs = wavread(load_filepath)
-    plot(
+    plt = plot(
         0:1/fs:(length(snd)-1)/fs,
         snd,
         xaxis="Time [s]",
         title="signal plot of $(basename(load_filepath))"
     )
+    display(plt)
 end
 
 function main()
@@ -23,19 +27,20 @@ function main()
     for load_file in readdir(data_path, join=true)[1:2]
         println(load_file)
         plot_snd(string(load_file))
+        wavplay(load_file)
         y, fs, nbits, chunk = wavread(load_file)
         y16 = reshape(convert.(Float16, y), (length(y),))
-        println(nbits, typeof(y16))
+        # mfccprintln(nbits, typeof(y16))
         # println(typeof(y))
         # mfcc(y, fs)
         data = spectrogram(y16)
-        heatmap(
+        hmp = heatmap(
             time(data),
             freq(data),
-            power(data),
+            log.(power(data)),
             title="spectrogram plot of $(basename(load_file))",
-            margin=10
         )
+        display(hmp)
     end
 end
 
